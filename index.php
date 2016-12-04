6a933aea152e2e73012e78a683fbe0909d083204
Comandos <00comandos@gmail.com>
2016-12-04 12:50:49 -0400

    Agregado publicidad (banner)

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
 	<?php include 'inc/head_common.php'; ?>	
 </head>
 <body>
+	<!--PUBLICIDAD PRENSA ALTERNATIVA-->
+	<section class="publicidad">
+		<a href="http://prensalternativa.com.ve/" target="_blank">
+			<img src="img/prensa-alternativa.png" alt="">
+			<img src="img/prensa-alternativa-mobile.png" alt="">
+		</a>
+	</section>
+
+
 	<?php include 'inc/header.php'; ?>
 	
 	<!--REPRODUCTOR-->
@@ -11,11 +20,25 @@
 		<iframe width="140%" height="85" allowTransparency="true" border="0" style="border: 0; box-shadow: 0px 0px 0  #fff;" src="http://radioshd.com/radioplayer/#goradiohd"></iframe>
 	</section>
 
+	<!--PUBLICIDAD CREACIONES OCEANO AZUL-->
+	<section class="publicidad">
+		<a href="https://www.instagram.com/creaciones_oceano/" target="_blank">
+			<img src="img/creaciones-oceano.png" alt="">
+			<img src="img/creaciones-oceano-mobile.png" alt="">
+		</a>
+	</section>
+
 	<!--REDES SOCIALES-->
 	<section class="socialMedia">
 		<!--TIMELINE-->
 		<div class="item">			
 			<a class="twitter-timeline" data-width="252" data-height="319" href="https://twitter.com/GoRadioHD">Tweets by GoRadioHD</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
+
+		<!--INSTAGRAM-->
+		<div class="item">
+			 <!-- LightWidget WIDGET -->
+			 <script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/1052c5a2e6885d7d9a725edd7087d8f6.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
 		</div>
 	</section>
 		
